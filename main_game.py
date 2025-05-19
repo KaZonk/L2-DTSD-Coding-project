3d95390c10c5c8d5e3be42e18c2f9e302b0a4b06
KaZonk <daok@stu.otc.school.nz>
2025-05-19 14:20:12 +1200

    I define the Game as class define the innit function within Game() create frames and containers
    make a dictionary for frames

--- main_game.py
+++ main_game.py
@@ -1,7 +1,63 @@
 """The beginning of something beautiful or terrible"""
 
-class Main_game():
+# Importing libraries
+import tkinter as tk
+from tkinter import ttk
+import random
+
+# Temporary useful link: https://www.digitalocean.com/community/tutorials/tkinter-working-with-classes
+
+class Game(tk.Tk):
+    """Initilising Main window as a class"""
+    def __init__(self, *args, **kwargs):
+        """using the innit function create the necesserary
+        widgets when Game is called"""
+        
+        self.wm_title("Click ball game")
+
+        # creating a container as a frame
+        container = tk.Frame(self, height=400, width=600)
+
+        container.pack(side="top", fill="both", expand=True)
+
+        # configuring the location of container using grid
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        # Create a dictionary of Frames
+        self.frames = {}
+
+        for F in (MainPage, SettingMenu, HelpMenu, AboutMenu):
+            """So, I will use a for loop to initallise these
+            class as frames so that they be raise and unraise
+            like pages"""
+            pass
+
+
+
+
+class MainPage():
+    def __init__(self):
+        pass
+
+    def start():
+        pass
     pass
 
-for i in range(3,11):
-    print(f"Week {i}")
+class SettingMenu():
+    def __init__(self):
+        pass
+
+    def quit():
+        pass
+    
+    pass
+
+class HelpMenu():
+    pass
+
+class AboutMenu():
+    pass
+
+if __name__ == "__main__":
+    pass
